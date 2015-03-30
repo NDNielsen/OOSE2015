11312c236f3d281f86a9f82016617e597212f37f
MieMoth <miemp123@gmail.com>
2015-03-30 15:50:13 +0200

    Modified code for Level class

--- src/com/oose/breakout/Level.java
+++ src/com/oose/breakout/Level.java
@@ -2,15 +2,15 @@ package com.oose.breakout;
 // Level: factory pattern
 public class Level {
 	public static Level GenerateLevel(int level_id, int amountOfBlock){
+		//For loop: Initialize blocks for Level 1:
 
 		return GenerateLevel(level_id, amountOfBlock);
+		
+		// Create the Levels
+		Level level_1 = new Level (1, 25);
 	}
 }
 
 
 
-//// Usage
-//Level level_1 = Level.GenerateLevel(1, 24) {
-//	//
-//}
 
