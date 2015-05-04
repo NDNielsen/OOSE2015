c66b1ae6178511a41aa2b06e50ce77beca7c7997
NDNielsen <ndni13@student.aau.dk>
2015-05-04 12:18:07 +0200

    Array index 20 -> 25

--- src/com/oose/breakout/Game.java
+++ src/com/oose/breakout/Game.java
@@ -22,7 +22,7 @@ public class Game extends BasicGame
 	
 	public Game(String gamename) {
 		super(gamename);
-		blocks = new Block[20];
+		blocks = new Block[25];
 	}
 
 	public static void main(String[] args)	{
